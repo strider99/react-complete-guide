3973fece001c5ff07524e5831d68e23f8bb405b3
strider99 <panigrahiomprakash@gmail.com>
2018-09-22 21:44:30 +0530

    flexible lists

--- src/App.js
+++ src/App.js
@@ -26,13 +26,21 @@ class App extends Component {
 
 
 
-  nameChangedHandler = (event) => {
+  nameChangedHandler = (event, id) => {
+    // Get the index for person id matching the passed id
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id == id;
+    });
+
+    // Get the person from the personIndex. we dont want to mutate the state directly so we use spread
+    const person = {...this.state.persons[personIndex]};
+
+    person.name = event.target.value;
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
     this.setState({
-      persons: [
-        {name: 'Omprakash', age: 28},
-        {name: event.target.value, age: 24},
-        {name: 'Meh', age: 21}
-      ]
+      persons: persons
     })
   }
 
@@ -71,7 +79,8 @@ class App extends Component {
              key={person.id}
              click={this.deletePersonHandler.bind(this,index)}
              name={person.name}
-             age={person.age} />
+             age={person.age}
+             changed={(event) =>this.nameChangedHandler(event, person.id)} />
           })}
           {/* <Person name={this.state.persons[0].name} age={this.state.persons[0].age} /> */}
 
